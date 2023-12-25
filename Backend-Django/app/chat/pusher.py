a5a8a00e15cce914043702a44f374232f26d5070
Vimalesh C T <pranovvimal30@gmail.com>
2023-12-25 20:39:09 +0530

    initial commit

---
+++ Backend-Django/app/chat/pusher.py
@@ -0,0 +1,9 @@
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='1729962',
+  key='ac3b0db13498da627e95',
+  secret='0fec56d14c8f949316f4',
+  cluster='ap2',
+  ssl=True
+)
---
+++ Backend-Django/app/chat/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .pusher import pusher_client
+# Create your views here.
+
+class messageAPIView(APIView):
+    def post(self,request):
+        pusher_client.trigger('chat', 'message', {
+            'username': request.data['username'],
+            'message':request.data['message'],
+        })
+        return Response([])
